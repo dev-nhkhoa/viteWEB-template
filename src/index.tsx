0c0369f6166256b273cf1384f0ceaca21b7773f9
nhkhoa.a <70632030+dev-nhkhoa@users.noreply.github.com>
2024-05-06 19:48:21 +0700

    update import <Router />

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
-import App from 'components/App'
+import Router from './Router'
 
 import './index.css'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(<Router />)
