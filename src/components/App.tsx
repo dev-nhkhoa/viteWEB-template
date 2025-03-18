45f0e56663015bf99a8de746b9c9f391e28eac65
nhkhoa <work.nhkhoa@gmail.com>
2025-03-18 20:08:17 +0700

    Revert "init"
    This reverts commit 80b291dd0aa5e62d529dad08f953d79d2d5590e1.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,5 @@
-import React from "react";
-import AddressSelector from "./AddressSelector";
-import "./App.css";
-
 function App() {
-  return (
-    <div className="app-container">
-      <header className="app-header">
-        <h1>Hệ thống chọn địa chỉ</h1>
-      </header>
-      <main className="app-main">
-        <AddressSelector />
-      </main>
-      <footer className="app-footer">
-        <p>&copy; {new Date().getFullYear()} - Hệ thống lựa chọn địa chỉ Việt Nam</p>
-      </footer>
-    </div>
-  );
+  return <div>App templatse</div>
 }
 
-export default App;
+export default App
