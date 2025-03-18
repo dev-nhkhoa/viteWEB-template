80b291dd0aa5e62d529dad08f953d79d2d5590e1
nhkhoa <work.nhkhoa@gmail.com>
2025-03-18 19:45:38 +0700

    init

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,21 @@
+import React from "react";
+import AddressSelector from "./AddressSelector";
+import "./App.css";
+
 function App() {
-  return <div>App template</div>
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Hệ thống chọn địa chỉ</h1>
+      </header>
+      <main className="app-main">
+        <AddressSelector />
+      </main>
+      <footer className="app-footer">
+        <p>&copy; {new Date().getFullYear()} - Hệ thống lựa chọn địa chỉ Việt Nam</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
